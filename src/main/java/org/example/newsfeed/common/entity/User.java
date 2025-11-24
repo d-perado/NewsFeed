040ca3016c5a9c5ce1b478e40800c715919406f2
d-perado <sjm01065@naver.com>
2025-11-24 09:31:36 +0900

    feat: User 엔티티추가
    User 엔티티추가

--- src/main/java/org/example/newsfeed/common/entity/User.java
+++ src/main/java/org/example/newsfeed/common/entity/User.java
@@ -1,4 +1,40 @@
 package org.example.newsfeed.common.entity;
 
-public class User {
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User extends TimeBaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nickname;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private boolean isDeleted = false;
+    @Column
+    private String introduction;
+
+    public User(String nickname, String email, String password, String introduction) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+        this.introduction = introduction;
+    }
+
+    public void modify(String email, String password, String introduction) {
+        this.email = email;
+        this.password = password;
+        this.introduction = introduction;
+    }
+
 }
