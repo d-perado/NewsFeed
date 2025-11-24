c9cc6c227a89d63b727cb1e28e6037d237a95894
remnantcjy <remnantcjy@gmail.com>
2025-11-24 12:43:06 +0900

    feat: FeedRepository에 @Repository 어노테이션 추가

--- src/main/java/org/example/newsfeed/domain/feed/repository/FeedRepository.java
+++ src/main/java/org/example/newsfeed/domain/feed/repository/FeedRepository.java
@@ -2,6 +2,8 @@ package org.example.newsfeed.domain.feed.repository;
 
 import org.example.newsfeed.common.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 }
