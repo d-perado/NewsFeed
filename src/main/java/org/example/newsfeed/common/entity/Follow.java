abca730999048ffb42ba6839e22a19f8f2500492
d-perado <sjm01065@naver.com>
2025-11-24 10:18:57 +0900

    feat: follow Entity 추가
    follow Entity 추가

--- src/main/java/org/example/newsfeed/common/entity/Follow.java
+++ src/main/java/org/example/newsfeed/common/entity/Follow.java
@@ -1,4 +1,22 @@
 package org.example.newsfeed.common.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Table(name = "follows")
+@Getter
 public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "followed_id")
+    private User followedId;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User followingId;
 }
