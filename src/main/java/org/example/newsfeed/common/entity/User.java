aa9199b785e5be6f5ef234d97c83b8ad36c1006b
bjh8130 <bjh8130@gmail.com>
2025-11-25 14:40:24 +0900

    docs: 브렌치 추가

--- src/main/java/org/example/newsfeed/common/entity/User.java
+++ src/main/java/org/example/newsfeed/common/entity/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Table(name = "users")
 @Getter
 @NoArgsConstructor
+
 public class User extends TimeBaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
