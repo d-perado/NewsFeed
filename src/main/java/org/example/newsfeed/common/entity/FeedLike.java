7b0e9ba478685614135fec36549f0595a23b513d
remnantcjy <remnantcjy@gmail.com>
2025-11-26 10:31:03 +0900

    refactor: 피드 생성, 수정, 삭제 리팩터링
    - 파라미터에 @AuthenticationPrincipal UserDetails user 추가
    - 수정, 삭제 시 작성자 본인이면 수정, 삭제 실행 / 아니면 예외처리 기능 구현

--- src/main/java/org/example/newsfeed/common/entity/FeedLike.java
+++ src/main/java/org/example/newsfeed/common/entity/FeedLike.java
@@ -1,4 +1,29 @@
 package org.example.newsfeed.common.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "feed_likes")
+@Getter
+@NoArgsConstructor
 public class FeedLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    public FeedLike(User user, Feed feed) {
+        this.user = user;
+        this.feed = feed;
+    }
 }
